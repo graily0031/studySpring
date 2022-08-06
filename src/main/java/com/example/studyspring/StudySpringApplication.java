0e2813d733b6fd7019ad2d090125e3661b163570
graily0031 <graily0031@gmail.com>
2022-08-07 00:35:16 +0900

    commit

--- src/main/java/com/example/studyspring/StudySpringApplication.java
+++ src/main/java/com/example/studyspring/StudySpringApplication.java
@@ -3,6 +3,8 @@ package com.example.studyspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.beans.ConstructorProperties;
+
 @SpringBootApplication
 public class StudySpringApplication {
 
