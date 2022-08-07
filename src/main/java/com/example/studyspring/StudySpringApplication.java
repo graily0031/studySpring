713bbd963fb0e432c4a0dcffbf163ec717b6d5f5
graily0031 <graily0031@gmail.com>
2022-08-07 11:21:24 +0900

    commit

--- src/main/java/com/example/studyspring/StudySpringApplication.java
+++ src/main/java/com/example/studyspring/StudySpringApplication.java
@@ -1,11 +1,12 @@
 package com.example.studyspring;
 
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.beans.ConstructorProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MybatisAutoConfiguration.class})
 public class StudySpringApplication {
 
     public static void main(String[] args) {
